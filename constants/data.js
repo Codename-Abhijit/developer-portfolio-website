6b3ab3295eef776f5a73f8cee374e4977228f6c2
Abhijit R <abhijit.r@breville.com>
2024-05-26 00:06:17 +0530

    Initial commit

---
+++ constants/data.js
@@ -0,0 +1,63 @@
+const userData = {
+  name: "Abhijit R",
+  designation: "Full Stack Developer",
+  avatarUrl: "",
+  email: "abhijit11997@gmail.com",
+  phone: "+91 9663211825",
+  address: "Bengaluru, Karnataka, India.",
+  projects: [
+    {
+      title: "Breville.com",
+      link: "https://www.breville.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "Beanz.com",
+      link: "https://www.Beanz.com",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Bp.com",
+      link: "https://www.bp.com",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "Castrol.com",
+      link: "https://www.castrol.com",
+      imgUrl: "/covidrescue.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `With over four years of experience as a Frontend Developer, I bring a diverse skill set to the table. Proficient in HTML5, CSS3, JavaScript, and advanced frameworks like React and Redux, I specialize in crafting robust and user-friendly web applications. My project experience includes developing crucial functionalities for Ecommerce websites, such as seamless cart and checkout processes, authentication, and payment integrations. I'm passionate about leveraging analytics solutions to drive data-informed decision-making, resulting in notable increases in order placement percentages and platform stability. Collaborating effectively with cross-functional teams, I thrive on contributing my expertise to innovative web development projects, pushing boundaries and creating impactful digital experiences.`,
+    ],
+    currentProject: "Breville",
+    currentProjectUrl: "https://www.breville.com",
+  },
+  experience: [
+    {
+      title: "Software Developer - 2",
+      company: "Nggawe Nirman Technologies (An Infogain Company)",
+      year: "2021",
+      companyLink: "https://mroads.com",
+      desc: "Developed multiple React components for essential functionalities on the Breville/Sage Kitchen appliances Ecommerce website. These components were deployed to the AEM server and are currently live in production.",
+    },
+    {
+      title: "Software Developer - 1",
+      company: "Wipro Limited",
+      year: "2019",
+      companyLink: "https://wipro.com",
+      desc: "Worked on development of Single Page Applications (SPAs) for BP and Castrol clients, focusing on delivering high-quality user experiences.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/18p_qOeefnVsgrUNEpuQXgE8qpXVOp2k_/view?usp=sharing",
+  socialLinks: {
+    linkedin: "https://www.linkedin.com/in/abhijit-r-a6057758",
+    github: "https://github.com/Codename-Abhijit",
+  },
+};
+
+export default userData;
